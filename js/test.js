97b070e7144ac7c121da046f3734cfee799f2bad
Vims <Jawaharbabu_d@yahoo.com>
2024-04-17 00:10:50 +0100

    first Commit

---
+++ js/test.js
@@ -0,0 +1,6 @@
+// JavaScript comment
+//Execute JavaScript Code when abutton is clicked
+document.getElementById("btnMyButton").addEventListener("click", function ()
+{
+    alert("Button Clicked!");
+});
